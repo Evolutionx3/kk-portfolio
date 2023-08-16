d07f88bdbbb3d24c9df87de67d55ecaf3bc560bc
Evolutionx3 <frontdav1707@gmail.com>
2023-08-16 20:38:17 +0200

    Fix CSS

--- src/components/atoms/TestimonialsText/TestimonialsText.jsx
+++ src/components/atoms/TestimonialsText/TestimonialsText.jsx
@@ -4,7 +4,6 @@ const TestimonialsText = () => {
   return (
     <h4 className="testimonials_text">
       "Katarzyna&apos;s skills in UI design are
-      <br />
       <span className="testimonials_text_featured">really exceptional...</span>"
     </h4>
   );
